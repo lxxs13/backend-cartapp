73afedacb97b07dc2322553c7575c917530cfdbb
lxxs13 <luis.quiroz90@gmail.com>
2023-07-02 12:03:39 -0600

    Correción del CrossOrigin

--- src/main/java/com/luis/backend/cartapp/backendcartapp/controllers/ProductController.java
+++ src/main/java/com/luis/backend/cartapp/backendcartapp/controllers/ProductController.java
@@ -3,6 +3,7 @@ package com.luis.backend.cartapp.backendcartapp.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,7 @@ import com.luis.backend.cartapp.backendcartapp.services.ProductService;
 import com.luis.backend.cartapp.backendcartapp.models.entities.Product;
 
 @RestController // es un API Rest; devuelve un JSON o XML
+@CrossOrigin(origins = "http://localhost:5173")
 // @RequestMapping("/api") //ruta base
 public class ProductController {
 
